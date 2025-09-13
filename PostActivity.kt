bbd5c2621aafcc35e8f496668e079dcbd0523125
Muhammad Hassaan Mudassar <i230536@isb.nu.edu.pk>
2025-09-13 16:39:28 +0500

    Add files via upload
    this is for add post page

---
+++ PostActivity.kt
@@ -0,0 +1,11 @@
+package com.hassaan.i230536_assignment1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.addpostpage) // this is your activity_post.xml
+    }
+}
