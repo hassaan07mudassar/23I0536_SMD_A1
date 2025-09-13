b696c45403436450fa8ec0dff5a88362c43eb83f
Muhammad Hassaan Mudassar <i230536@isb.nu.edu.pk>
2025-09-13 16:30:03 +0500

    Add files via upload
    it helps open the chat when profile is clicked

---
+++ DirectMessagesActivity.kt
@@ -0,0 +1,25 @@
+package com.hassaan.i230536_assignment1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class DirectMessagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.messagespage)
+
+        val backArrow: ImageView = findViewById(R.id.back_arrow)
+        backArrow.setOnClickListener { finish() }
+
+        // Example: open chat when Joshua’s chat is clicked
+        val chatJoshua: LinearLayout = findViewById(R.id.chat_joshua)
+        chatJoshua.setOnClickListener {
+            val intent = Intent(this, ChatScreenActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
