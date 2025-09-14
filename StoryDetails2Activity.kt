9d5c3904320539b5db97eccff0c5976ef5cd81a1
Muhammad Hassaan Mudassar <i230536@isb.nu.edu.pk>
2025-09-14 07:34:01 +0500

    Add files via upload

---
+++ StoryDetails2Activity.kt
@@ -0,0 +1,11 @@
+package com.hassaan.i230536_assignment1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StoryDetails2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.storydetails2)
+    }
+}
